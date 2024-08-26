d1ae752a16acd181749ee5537d454dd3318c0e1a
Jed Tiotuico <jed.tiotuico@gmail.com>
2024-08-26 17:39:44 +0800

    Initial Commit

---
+++ MyTodoList/MyTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodoListApp.swift
+//  MyTodoList
+//
+//  Created by Jed Tiotuico on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MyTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
